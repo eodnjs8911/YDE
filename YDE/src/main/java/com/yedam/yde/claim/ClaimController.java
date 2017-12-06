730e0f58d36bb1ce733382a4711efdb4a0d35891
xboymir <xboymir@hanmir.com>
2017-12-06 15:20:44 +0900

    claim, mapper, config 수정
    claim, mapper, config 수정

---
+++ YDE/src/main/java/com/yedam/yde/claim/ClaimController.java
@@ -0,0 +1,29 @@
+package com.yedam.yde.claim;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class ClaimController {
+
+	@Autowired
+	ClaimService claimService;
+	
+	@RequestMapping("/claim/selectOne.do")
+	public String selectOne(ClaimVO vo, Model model) {
+		System.out.println("[ClaimController][selectOne]");
+		model.addAttribute("claim", claimService.selectOne(vo));
+		
+		return "claim/selectOne";
+	}
+	
+	@RequestMapping("/claim/selectList.do")
+	public String selectList(Model model) {
+		System.out.println("[ClaimController][selectList]");
+		model.addAttribute("claimList", claimService.selectList());
+		
+		return "claim/selectOne";
+	}
+}

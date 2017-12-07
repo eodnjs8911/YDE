a583f3094c7f47bbfe2b3dcb313b7af0fe5146f8
eodnjs8911 <eodnjs8911@naver.com>
2017-12-07 14:25:28 +0900

    sms express update

--- YDE/src/main/java/com/yedam/yde/sms/SmsController.java
+++ YDE/src/main/java/com/yedam/yde/sms/SmsController.java
@@ -1,26 +1,44 @@
 package com.yedam.yde.sms;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class SmsController {
 	@Autowired
 	SmsService smsService;
-	
-	@RequestMapping("/sms/selectOne.do")
-	public String selectOne(SmsVO vo, Model model) {
-		System.out.println("[SmsController][selectOne]");
-		model.addAttribute("sms",smsService.selectOne(vo));
-		return "sms/selectOne";
+
+	@RequestMapping("/sms/insert.do")
+	public void insert(SmsVO vo) {
+		System.out.println("[SmsController][insert]");
 	}
-	
+
+	@RequestMapping("/sms/update.do")
+	public void update(SmsVO vo) {
+		System.out.println("[SmsController][update]");
+	}
+
+	@RequestMapping("/sms/delete.do")
+	public void delete(SmsVO vo) {
+		System.out.println("[SmsController][delete]");
+	}
+
+	@RequestMapping("/sms/selectOne.do")
+	@ResponseBody
+	public SmsVO selectOne(SmsVO vo, Model model) {
+		System.out.println("[SmsController][selectOne]");
+		return smsService.selectOne(vo);
+	}
+
 	@RequestMapping("/sms/selectList.do")
-	public String selectList(Model model) {
+	@ResponseBody
+	public List<SmsVO> selectList(Model model) {
 		System.out.println("[SmsController][selectList]");
-		model.addAttribute("smsList",smsService.selectList());
-		return "sms/selectList";
+		return smsService.selectList();
 	}
 }

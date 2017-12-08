a791538a17253061b34d561fb88fb017190fb133
User <User@YD01-08>
2017-12-08 15:10:18 +0900

    md1

---
+++ YDE/src/main/java/com/yedam/yde/MD1/MD1Controller.java
@@ -0,0 +1,13 @@
+package com.yedam.yde.MD1;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.yedam.yde.claim.ClaimVO;
+@Controller
+@RequestMapping("/MD1/")
+public class MD1Controller {
+
+	
+}

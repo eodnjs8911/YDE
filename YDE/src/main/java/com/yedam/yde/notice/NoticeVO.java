8464e9251daabd0660db4d81d6bbb366aed314d8
Daesik <Daesik@DESKTOP-9I811IV>
2017-12-17 10:03:21 +0900

    epilogue dao생성

--- YDE/src/main/java/com/yedam/yde/notice/NoticeVO.java
+++ YDE/src/main/java/com/yedam/yde/notice/NoticeVO.java
@@ -3,7 +3,33 @@ package com.yedam.yde.notice;
 import java.sql.Date;
 
 public class NoticeVO {
-	int notice_no;
-	String notice_date;
-	String notice_content;
+	int noticeNo;
+	String noticeDate;
+	String noticeContent;
+	
+	public int getNoticeNo() {
+		return noticeNo;
+	}
+	public void setNoticeNo(int noticeNo) {
+		this.noticeNo = noticeNo;
+	}
+	public String getNoticeDate() {
+		return noticeDate;
+	}
+	public void setNoticeDate(String noticeDate) {
+		this.noticeDate = noticeDate;
+	}
+	public String getNoticeContent() {
+		return noticeContent;
+	}
+	public void setNoticeContent(String noticeContent) {
+		this.noticeContent = noticeContent;
+	}
+	
+	@Override
+	public String toString() {
+		return "NoticeVO [noticeNo=" + noticeNo + ", noticeDate=" + noticeDate + ", noticeContent=" + noticeContent
+				+ "]";
+	}
+
 }

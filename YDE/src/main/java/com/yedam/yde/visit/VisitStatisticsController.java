4e6be6e558e55cbdc46a9c3f4dd57d4d9f527076
xboymir <xboymir@hanmir.com>
2017-12-19 16:57:30 +0900

    171219 visit 통계 작업
    171219 visit 통계 작업

---
+++ YDE/src/main/java/com/yedam/yde/visit/VisitStatisticsController.java
@@ -0,0 +1,47 @@
+package com.yedam.yde.visit;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+
+
+@Controller
+@RequestMapping("/Visit/")
+public class VisitStatisticsController {
+
+	@Autowired
+	VisitStatisticsService visitStatisticsService;
+
+	@ModelAttribute("conditionMap")
+	public Map<String, String> SearchConditionMap() {
+		Map<String, String> conditionMap = new HashMap<String, String>();
+		conditionMap.put("방문일", "VISIT_DATE");
+		conditionMap.put("IP", "VISIT_IP");
+		return conditionMap;
+	}
+	
+/*	// 목록 (ajax)
+	@RequestMapping("getVisitStatisticsList.do")
+	@ResponseBody // JSON 또는 XML로 변환
+	public List<VisitStatisticsSearchVO> getVisitStatisticsList() {
+
+		return VisitStatisticsSearchVO.getVisitStatisticsList(null);
+	}*/
+	
+	// 목록 (ajax)
+		@RequestMapping("getVisitStatisticsList.do")
+		@ResponseBody 
+		public String getVisitStatisticsList(VisitStatisticsSearchVO statistics, Model model) {
+			model.addAttribute("visitStatisticsList", visitStatisticsService.getVisitStatisticsList(statistics));
+			return "visit/visitStatisticsList";
+		}
+
+}
---
+++ YDE/src/main/java/com/yedam/yde/visit/VisitStatisticsVO.java
@@ -0,0 +1,21 @@
+package com.yedam.yde.visit;
+
+public class VisitStatisticsVO {
+
+	String visitDate;
+	String visitIp;
+	
+	public String getVisitDate() {
+		return visitDate;
+	}
+	public void setVisitDate(String visitDate) {
+		this.visitDate = visitDate;
+	}
+	public String getVisitIp() {
+		return visitIp;
+	}
+	public void setVisitIp(String visitIp) {
+		this.visitIp = visitIp;
+	}
+	
+}
